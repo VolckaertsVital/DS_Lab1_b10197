b258c060a1dd4339dbb9c9d6ee602909bd27a7fc
Asif Wasefi <asifwasefi@yahoo.com>
2022-03-16 12:20:55 +0100

    Client socket file transfer

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,70 +1,38 @@
-// A Java program for a Client
-import java.net.*;
 import java.io.*;
+import java.net.Socket;
 
-public class Client
-{
-    // initialize socket and input output streams
-    private Socket socket		 = null;
-    private DataInputStream input = null;
-    private DataOutputStream out	 = null;
+public class Client {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
 
-    // constructor to put ip address and port
-    public Client(String address, int port)
-    {
-        // establish a connection
-        try
-        {
-            socket = new Socket(address, port);
-            System.out.println("Connected");
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("localhost",5000)) {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-            // takes input from terminal
-            input = new DataInputStream(System.in);
+            sendFile("path/to/file1.pdf");
+            sendFile("path/to/file2.pdf");
 
-            // sends output to the socket
-            out = new DataOutputStream(socket.getOutputStream());
-        }
-        catch(UnknownHostException u)
-        {
-            System.out.println(u);
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
-        }
-
-        // string to read message from input
-        String line = "";
-
-        // keep reading until "Over" is input
-        while (!line.equals("Over"))
-        {
-            try
-            {
-                line = input.readLine();
-                out.writeUTF(line);
-            }
-            catch(IOException i)
-            {
-                System.out.println(i);
-            }
-        }
-
-        // close the connection
-        try
-        {
-            input.close();
-            out.close();
-            socket.close();
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
+            dataInputStream.close();
+            dataInputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
-    public static void main(String args[])
-    {
-        Client client = new Client("127.0.0.1", 5000);
+    private static void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // send file size
+        dataOutputStream.writeLong(file.length());
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            dataOutputStream.write(buffer,0,bytes);
+            dataOutputStream.flush();
+        }
+        fileInputStream.close();
     }
 }
