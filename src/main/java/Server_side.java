2de7958a91eb7b581aeb7627bce9d989ef74da4e
Vital <vitalv@skynet.be>
2022-03-16 12:04:23 +0100

    Server_side

---
+++ src/main/java/Server_side.java
@@ -0,0 +1,58 @@
+import java.net.*;
+import java.io.*;
+
+public class Server_side {
+    private Socket		 socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in	 = null;
+
+    public Server_side(int port)
+    {
+        // starts server and waits for a connection
+        try
+        {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+
+            System.out.println("Waiting for a client ...");
+
+            socket = server.accept();
+            System.out.println("Client accepted");
+
+            // takes input from the client socket
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+
+            // reads message from client until "Over" is sent
+            while (!line.equals("Over"))
+            {
+                try
+                {
+                    line = in.readUTF();
+                    System.out.println(line);
+
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            // close connection
+            socket.close();
+            in.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Server_side server = new Server_side(5000);
+    }
+}
